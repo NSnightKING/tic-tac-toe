1cafe79950b0ede0013bc10af27ad947a7ababe3
NSnightKING <nabin005.rman@gmail.com>
2023-07-03 13:01:26 +0545

    hi

---
+++ src/components/login.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle login logic is here
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+     
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default LoginPage;
---
+++ src/components/ttt.jsx
@@ -0,0 +1,107 @@
+
+
+import React, { useState } from 'react';
+
+const Square = ({ value, onClick }) => (
+  <button onClick={onClick}>
+    {value}
+  </button>
+);
+
+
+
+
+const Board = () => {
+   // initial value of all square
+  const [squares, setSquares] = useState(Array(9).fill(null));
+   // getting first player start with (X)
+  const [xIsNext, setXIsNext] = useState(true);
+
+
+
+
+  // indexing each square with value (X/O). here index is define as (i). 
+  const handleClick = i => {
+    const newSquares = squares.slice();
+    // on the user click it shows either (X) or (O) depending upon state
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    // new state where value is changed
+    setSquares(newSquares);
+     // after the (X) turn next turn goes to (O). It work as like loop 
+    setXIsNext(!xIsNext);
+  };
+
+
+
+
+  // rendering squares through click to track the user where he/she is on
+  const renderSquare = i => (
+    <Square
+      value={squares[i]}
+      onClick={() => handleClick(i)}
+    />
+  );
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <div>
+      <div>{status}</div>
+      <div>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
+
+
+
+
+
+
+  const calculateWinner = squares => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  
+  return null;
+};
+
+
